81901dd07936703400a7e223049151d588938467
kitusneDamik <danilllyk2@gmail.com>
2021-02-01 15:53:59 +0600

    Files for Msp Exam

---
+++ ExamMsp/ExamMsp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamMsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
